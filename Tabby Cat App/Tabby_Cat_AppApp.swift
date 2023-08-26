3b7ad096b66a8df0e049168d68f0832ecdffe40c
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-08-26 12:17:13 +0800

    Initial Commit

---
+++ Tabby Cat App/Tabby_Cat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tabby_Cat_AppApp.swift
+//  Tabby Cat App
+//
+//  Created by Celine Quek on 26/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Tabby_Cat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
